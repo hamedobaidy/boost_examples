3ec06bbabc4053c98a9f0088402bc82ad6cbac26
hamedobaidy <hamed.obaidy@gmail.com>
2015-08-08 15:14:08 +0430

    format

--- boost_examples/src/bernouli_numbers_example.cpp
+++ boost_examples/src/bernouli_numbers_example.cpp
@@ -13,15 +13,13 @@ int main() {
 	std::vector<float> bn; // Space for 32-bit `float` precision Bernoulli numbers.
 
 	// Start with Bernoulli number 0.
-	boost::math::bernoulli_b2n<float>(0, 32, std::back_inserter(bn)); // Fill vector with even Bernoulli numbers.
+	// Fill vector with even Bernoulli numbers.
+	boost::math::bernoulli_b2n<float>(0, 32, std::back_inserter(bn));
 
-	for(size_t i = 0; i < bn.size(); i++)
-	{ // Show vector of even Bernoulli numbers, showing all significant decimal digits.
-	    std::cout << std::setprecision(std::numeric_limits<float>::digits10)
-	        << i*2 << ' '
-	        << bn[i]
-	        << std::endl;
+	for (size_t i = 0; i < bn.size(); i++) {
+		// Show vector of even Bernoulli numbers, showing all significant decimal digits.
+		std::cout << std::setprecision(std::numeric_limits<float>::digits10)
+				<< i * 2 << ' ' << bn[i] << std::endl;
 	}
 }
-
 
